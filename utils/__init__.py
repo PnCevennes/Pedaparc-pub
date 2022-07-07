cece8f73e53a8d8d258e7d0ec1f0993c11f2d3ee
Alexandre-Roussel48 <alexandre.roussel.48@gmail.com>
2022-07-07 15:35:19 +0200

    Fix faille XSS

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,14 +2,19 @@ import re
 
 import markdown
 
-from bs4 import BeautifulSoup
+import bleach
 
 import config
 
 def render_page(md_content):
-    out = markdown.markdown(md_content)
+    out = markdown.markdown(sanitize_text(md_content))
     out = re.sub('a href="http', 'a target="_blank" href="http', out)
     return out
+
+
+def sanitize_text(text):
+    return bleach.clean(text)
+
 
 def format(formatage,tags):
     '''
