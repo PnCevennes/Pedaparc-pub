44cf60eaa12709b6d6af31a07af80a3f88a30bb9
Alexandre-Roussel48 <alexandre.roussel.48@gmail.com>
2022-07-20 16:29:34 +0200

    Ajout de la fonctionnalité admin suppression de mots-clés

--- models/thesaurus/utils.py
+++ models/thesaurus/utils.py
@@ -1,11 +1,15 @@
 '''
 Fichier contenant les fonctions utiles à la manipulation du thesaurus.
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 Fonctions:
 
-   get_from_thes(idref=None, *, nom=None) -> Thesaurus[]
-   create_thesaurus(data) -> Thesaurus
-   get_thesaurus(id_thes) -> Thesaurus
+   | get_from_thes(idref=None, nom=None) -> Thesaurus[]
+   | create_thesaurus(data) -> Thesaurus
+   | delete_thesaurus(id_thes)
+   | get_thesaurus(id_thes) -> Thesaurus
+   | get_pictos() -> Thesaurus[]
+   | get_all_thes() -> Thesaurus[]
 '''
 
 from models.thesaurus.thesaurus import Thesaurus
@@ -18,14 +22,11 @@ def get_from_thes(idref=None, *, nom=None):
     a été passé en paramètre.
 
         Param(s):
-                idref (int)(facultatif): Id de référence du type d'objets
-                Thesaurus à renvoyer
-                nom (str)(facultatif): nom de référence du type d'objets
-                Thesaurus à renvoyer
+                | idref (int)(facultatif): Id de référence du type d'objets Thesaurus à renvoyer
+                | nom (str)(facultatif): nom de référence du type d'objets Thesaurus à renvoyer
 
         Return(s):
-                thes (Thesaurus[]): Liste de tous les objets Thesaurus
-                correspondants au type d'objets passé en paramètre
+                | thes (Thesaurus[]): Liste de tous les objets Thesaurus correspondants au type d'objets passé en paramètre
     '''
     if idref is None: 
         if nom:
@@ -43,11 +44,10 @@ def create_thesaurus(data):
     Crée un objet Thesaurus à partir des données passées en paramètre.
 
         Param(s):
-                data ({}): Dictionnaire contenant les attributs de l'objet
+                | data ({}): Dictionnaire contenant les attributs de l'objet
 
         Return(s):
-                thes (Thesaurus): Objet Thesaurus dont
-                les attributs ont été remplis
+                | thes (Thesaurus): Objet Thesaurus dont les attributs ont été remplis
     '''
     thes = Thesaurus.create(
         reference = data['reference'],
@@ -55,16 +55,25 @@ def create_thesaurus(data):
     return thes
 
 
+def delete_thesaurus(id_thes):
+    '''
+    Supprime l'objet Thesaurus dont l'id a été passé en paramètre.
+
+        Param(s):
+                | id_thes (int): Id de l'objet Thesaurus à supprimer
+    '''
+    Thesaurus.delete().where(Thesaurus.id==id_thes).execute()
+
+
 def get_thesaurus(id_thes):
     '''
     Retourne l'objet Thesaurus dont l'Id a été passé en paramètre.
 
         Param(s):
-                id_thes (int): Id de l'objet Thesaurus à renvoyer
+                | id_thes (int): Id de l'objet Thesaurus à renvoyer
 
         Return(s):
-                thes (Thesaurus): Objet Thesaurus dont l'Id a été
-                passé en paramètre
+                | thes (Thesaurus): Objet Thesaurus dont l'Id a été passé en paramètre
     '''
     return Thesaurus.get(id=id_thes)
 
@@ -74,7 +83,16 @@ def get_pictos():
     Retourne les objets Thesaurus associés à des pictogrammes.
 
         Return(s):
-                thes (Thesaurus[]): Liste de tous les objets Thesaurus
-                associés à un pictogramme
+                | thes (Thesaurus[]): Liste de tous les objets Thesaurus associés à un pictogramme
     '''
     return Thesaurus.select().where(Thesaurus.reference.in_([1,4,5]))[:]
+
+
+def get_all_thes():
+    '''
+    Retourne tous les objets Thesaurus qui ne sont pas des catégories.
+
+        Return(s):
+                | thes (Thesaurus[]): Liste de tous les objets Thesaurus qui ne sont pas des catégories
+    '''
+    return Thesaurus.select().where(Thesaurus.reference!=0)[:]
--- views/thesaurus.py
+++ views/thesaurus.py
@@ -17,21 +17,32 @@ views = Blueprint('thesaurus',__name__)
 @auth.require_valid_user
 @auth.require_admin_user
 def thesaurus_create_form():
-    return render_template('/thesaurus/create/thesaurus_create_form.htm', 
-        references = thesaurus.get_from_thes(idref='0'))
+    return render_template('/thesaurus/thesaurus_admin.htm', 
+        references = thesaurus.get_from_thes(idref='0'),
+        tags = thesaurus.get_all_thes())
 
 
 #Cette fonction permet de créer le nouveau tag
-@views.route('/create', methods = ['POST'])
+@views.route('/create', methods=['POST'])
 @auth.require_valid_user
 @auth.require_admin_user
-def thesaurus_create_create_tag():
+def thesaurus_create_tag():
     data = request.form
     try:
         thesaurus.create_thesaurus(data)
         return redirect('/')
     except Exception:
         return render_template(
-            '/thesaurus/create/thesaurus_create_form.htm',
+            '/thesaurus/thesaurus_admin.htm',
             references = thesaurus.get_from_thes(idref='0'), 
             alert = data['nom'])
+
+
+#Cette fonction permet de supprimer un tag
+@views.route('/delete', methods=['POST'])
+@auth.require_valid_user
+@auth.require_admin_user
+def thesaurus_delete_tag():
+    id_thes = request.form['id_thes']
+    thesaurus.delete_thesaurus(id_thes)
+    return redirect('/')
